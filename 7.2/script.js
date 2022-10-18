ab2d42911062d6d13ea55beea4ef32c1c75ef226
NouamanGuendoul <113364960+NouamanGuendoul@users.noreply.github.com>
2022-10-18 14:24:21 +0200

    Add files via upload

---
+++ 7.2/script.js
@@ -0,0 +1,26 @@
+
+
+ function opdracht72(){
+var budget = prompt("Hoeveel kost het product dat je wilt kopen?");
+var product = 60;
+
+    
+  if(budget > product)
+  
+  {
+   alert("u heeft genoeg geld");
+    alert("fijne dag");
+ 
+  }
+
+  else {
+    
+   alert("u heeft niet genoeg geld");
+   alert("fijne dag");
+}
+ }
+
+ 
+
+
+
