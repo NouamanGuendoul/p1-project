232ea8df9aed75e01c27a6111038c1a4769b68d4
NouamanGuendoul <113364960+NouamanGuendoul@users.noreply.github.com>
2022-11-17 10:27:00 +0100

    Add files via upload

---
+++ php1/opdracht2b.php
@@ -0,0 +1,17 @@
+<?php
+
+$dag = date(" d , N , z , j ,  F , Y")
+
+echo "het is vandaag: $dag";
+
+$jaar = date ("Y");
+getal = 1;
+
+if($jaar >= $getal){
+  echo "het $jaar is een schrikeljaar ";
+}
+else
+{
+    echo "het $jaar is geen schrikeljaar ";
+}
+?>
