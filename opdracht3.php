ab3f3f5f619b35e646a4e5ddebbacbb9181c6a04
NouamanGuendoul <113364960+NouamanGuendoul@users.noreply.github.com>
2022-11-18 10:31:16 +0100

    Add files via upload

---
+++ opdracht3.php
@@ -0,0 +1,28 @@
+<?php
+
+$tekst1 = "<h1>vrijdag</h1>";
+
+$tekst2 = "<p>vrijdag</p>";
+
+echo "$tekst1";
+
+
+echo "$tekst2 ";
+
+$getal1 = 5;
+
+$getal2 = 10;
+
+echo "$getal1</br>";
+
+
+echo "$getal2";
+
+
+
+
+
+
+
+
+?>
