7d60991faea369faeed0346d36ff1c01aaf00f47
NouamanGuendoul <113364960+NouamanGuendoul@users.noreply.github.com>
2022-11-18 10:33:27 +0100

    Add files via upload

--- opdracht4.php
+++ opdracht4.php
@@ -1,13 +1,28 @@
 <?php
-$uur
-$uur = 1;
 
-if(){
+$uur = date("H");
 
+echo $uur;
+
+if($uur > 6 & $uur <12)
+
+{
+    echo "Het is ochtend";
 }
 
+elseif($uur > 11 & $uur <18)
+{
+    echo "Het is middag";
+}
 
+elseif($uur > 18 & $uur <24) 
+{
+    echo "Het is avond";
+}
 
-
+elseif($uur > 24 & $uur <6)
+{
+    echo "Het is nacht";
+}
 
 ?>
